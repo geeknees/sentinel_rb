89d61d374d70258f009a646ce79424cdc9c9bdb7
Masumi Kawasaki <geeknees@gmail.com>
2025-06-07 00:11:16 +0900

    Init

---
+++ lib/sentinel_rb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sentinel_rb/version"
+
+module SentinelRb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sentinel_rb/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SentinelRb
+  VERSION = "0.1.0"
+end
